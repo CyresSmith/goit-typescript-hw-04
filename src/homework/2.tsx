0396c64e371c9546ac226b8c0ae627f0a1866c9c
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-12-19 13:39:06 +0200

    done

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,15 @@
-import React, {useReducer} from "react";
+import { useReducer } from 'react';
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: 'start' | 'pending' | 'finished' | 'idle';
+};
+
+type Action =
+  | { type: 'START_REQUEST' }
+  | { type: 'PENDING_REQUEST' }
+  | { type: 'FINISH_REQUEST' }
+  | { type: 'RESET_REQUEST' };
 
 const initialState: State = {
   isRequestInProgress: false,
@@ -21,14 +32,17 @@ function requestReducer(state: State, action: Action): State {
 }
 
 export function RequestComponent() {
-  const [requestState, requestDispatch] = useReducer(requestReducer, initialState);
+  const [requestState, requestDispatch] = useReducer(
+    requestReducer,
+    initialState
+  );
 
   const startRequest = () => {
     requestDispatch({ type: 'START_REQUEST' });
-    // Імітуємо запит до сервера
+
     setTimeout(() => {
       requestDispatch({ type: 'PENDING_REQUEST' });
-      // Імітуємо отримання відповіді від сервера
+
       setTimeout(() => {
         requestDispatch({ type: 'FINISH_REQUEST' });
       }, 2000);
